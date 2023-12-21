5c754043f90154fd65816412410ff0eddc96d616
rootabeta <rootabeta@protonmail.com>
2023-12-21 00:05:18 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,143 @@
+use anyhow::Error;
+use serde::Deserialize;
+use serde_xml_rs::from_str;
+use std::thread::sleep;
+use std::time::Duration;
+use toml::Table;
+use ureq::Agent;
+
+pub struct Nation { 
+    pub nation: String,
+    pub password: String
+}
+
+pub struct Config { 
+    pub main_nation: String,
+    pub nations: Vec<Nation>
+}
+
+fn canonicalize(string: &str) -> String {
+    let mut output = String::from(string);
+    output.make_ascii_lowercase();
+    return str::replace(output.as_str(), " ", "_");
+}
+
+pub fn load_config(config_file: &str) -> Result<Config, Error> { 
+    let config_contents = std::fs::read_to_string(config_file)?;
+    let raw_config: Table = toml::from_str(&config_contents)?;
+    let mut nations = Vec::new();
+
+    let main_nation = raw_config
+        .get("config")
+        .expect("Did not find [config] block in config.toml!")
+        .get("main_nation")
+        .expect("Did not find main_nation in config.toml!")
+        .as_str()
+        .expect("Could not unwrap main_nation as string!")
+        .to_string();
+        
+    println!("Got main nation \"{0}\"", main_nation);
+
+
+    let puppets = raw_config.get("puppets")
+        .expect("Did not find [puppets] block in config.toml!")
+        .as_table()
+        .expect("Could not parse puppets as table!");
+
+    for puppet in puppets.keys() { 
+        let nation = puppet;
+        let password = puppets.get(puppet).unwrap().as_str().unwrap();
+
+        let new_nation: Nation = Nation{
+            nation: canonicalize(&nation.to_string()),
+            password: password.to_string()
+        };
+
+        nations.push(new_nation);
+    }
+
+    let config: Config = Config{
+        main_nation,
+        nations
+    };
+
+    Ok(config)
+}
+
+#[derive(Deserialize)]
+struct Issue { 
+    id: u32
+}
+
+#[derive(Deserialize)]
+struct Issues { 
+    #[serde(alias="ISSUE")]
+    issue: Vec<Issue>
+}
+
+#[derive(Deserialize)]
+struct NationResponse { 
+    #[serde(alias="ISSUES")]
+    issues: Issues
+}
+
+#[derive(Deserialize)]
+struct Packs { 
+    #[serde(alias="PACKS")]
+    packs: u32
+}
+
+pub fn get_issues(agent: &Agent, nation: &str, password: &str) -> Result<Option<Vec<u32>>, Error> { 
+    let mut issue_ids = Vec::new();
+
+    let url = format!(
+        "https://www.nationstates.net/cgi-bin/api.cgi?q=issues&nation={}",
+        nation
+    );
+
+    let response = agent
+        .get(&url)
+        .set("X-Password", password)
+        .call()?
+        .into_string()?;
+
+    let response = from_str(&response);
+
+    if response.is_ok() { 
+        let response: NationResponse = response.unwrap();
+
+        for issue in response.issues.issue { 
+            issue_ids.push(issue.id);
+        }
+        // Rate limit
+        sleep(
+            Duration::from_millis(750)
+        );
+
+        Ok(Some(issue_ids))
+    } else { 
+        Ok(None)
+    }
+}
+
+pub fn get_packs(agent: &Agent, nation: &str, password: &str) -> Result<u32, Error> { 
+    let url = format!(
+        "https://www.nationstates.net/cgi-bin/api.cgi?q=packs&nation={}",
+        nation
+    );
+
+    let response = agent
+        .get(&url)
+        .set("X-Password", password)
+        .call()?
+        .into_string()?;
+
+    let response: Packs = from_str(&response)?;
+
+    // Rate limit
+    sleep(
+        Duration::from_millis(750)
+    ); 
+
+    Ok(response.packs)
+}
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use anyhow::Error;
+use inquire::Select;
+use std::fs::File;
+use std::io::Write;
+use std::time::Duration;
+use magician::{Config, get_issues, get_packs, load_config};
+use ureq::{Agent, AgentBuilder};
+
+fn do_issues(config: Config, agent: Agent) -> Result<String, Error> { 
+    let filename = "issues.html";
+    let mut file = File::create(filename)?;
+
+    for nation in config.nations { 
+        let issues = get_issues(&agent, &nation.nation, &nation.password)?;
+        if issues.is_some() { 
+            for issue in issues.unwrap() { 
+                let issue_link = format!(
+                    "https://www.nationstates.net/container={}/template-overall=none/page=show_dilemma/dilemma={}",
+                    nation.nation,
+                    issue
+                );
+
+                let issue_link = format!("<div class=issue><a class=\"login_link\" onclick=\"document.getElementsByClassName('issue')[0].remove();\" target=\"_blank\" href={}>{} ({})</a><br></div>",
+                    issue_link,
+                    nation.nation,
+                    issue
+                );
+
+                writeln!(file, "{}", issue_link)?;
+            }
+        }
+        println!("Fetched issues for {0}", nation.nation);
+    }
+
+    Ok(filename.to_string())
+}
+
+fn do_packs(config: Config, agent: Agent) -> Result<String, Error> { 
+    let filename = "packs.html";
+    let mut file = File::create(filename)?;
+    for nation in config.nations { 
+        let packs = get_packs(&agent, &nation.nation, &nation.password)?;
+        if packs > 0 { 
+            let pack_link = format!(
+                        "https://www.nationstates.net/container={}/template-overall=none/page=deck",
+                        nation.nation,
+                    );
+
+            let pack_link = format!("<div class=packs><a class=\"login_link\" onclick=\"document.getElementsByClassName('packs')[0].remove();\" target=\"_blank\" href={}>{} ({})</a><br></div>",
+                pack_link,
+                nation.nation,
+                packs
+            );
+
+            let _ = writeln!(file, "{}", pack_link);
+        }
+        println!("Fetched {0} packs for {1}", packs, nation.nation);
+    }
+
+    Ok(filename.to_string())
+}
+
+fn do_logins(config: Config) -> Result<String, Error> { 
+    let filename = "logins.html";
+    let containers = "containerize.txt";
+    let mut file = File::create(filename)?;
+    let mut containers = File::create(containers)?;
+
+    for nation in config.nations { 
+        let container_rule = format!(
+            "@^.*\\.nationstates\\.net/(.*/)?container={}(/.*)?$ , {}",
+            nation.nation, 
+            nation.nation
+        );
+
+        let login_link = format!(
+            "https://www.nationstates.net/container={}/template-overall=none/page=login",
+            nation.nation
+        );
+
+        let nation_line = format!("<div class=puppet><a class=\"login_link\" onclick=\"document.getElementsByClassName('puppet')[0].remove();\" target=\"_blank\" href={}>{}</a><br></div>",
+            login_link,
+            nation.nation
+        );
+
+        writeln!(file, "{}", nation_line)?;
+        writeln!(containers, "{}", container_rule)?;
+    }
+
+    println!("Generated containerize rules at containerize.txt");
+    Ok(filename.to_string())
+}
+
+fn main() {
+    let Ok(config) = load_config("config.toml") else { 
+        panic!("Could not load configuration file!")
+    };
+
+    println!("Welcome, {}", config.main_nation);
+
+    let options: Vec<&str> = vec!["Answer Issues", "Open Packs", "Log In", "Exit"];
+    let Ok(mode) = Select::new("Operating mode?", options).prompt() else { 
+        panic!("Unrecognized option!");
+    };
+
+    let user_agent = format!(
+        "Magician/{0} (Developed by nation=Volstrostia; In use by nation={1})", 
+        env!("CARGO_PKG_VERSION"),
+        config.main_nation
+    );
+
+    let api_agent: Agent = AgentBuilder::new()
+        .user_agent(&user_agent)
+        .timeout(Duration::from_secs(5))
+        .build();
+
+    let outfile = match mode { 
+        "Answer Issues" => do_issues(config, api_agent), 
+        "Open Packs" => do_packs(config, api_agent), 
+        "Log In" => do_logins(config),  
+        "Exit" => { 
+            println!("Goodbye");
+            return;
+        }
+        _ => panic!("Unrecognized option!")
+    };
+
+    match outfile { 
+        Ok(outfile) => println!("Generated sheet at {outfile}"),
+        Err(reason) => panic!("Failed to generate sheet: {reason}")
+    };
+}
